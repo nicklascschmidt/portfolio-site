989ad8a241486139e8d7cd8a1fc1672c32341cf9
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2020-06-26 14:13:22 -0400

    updated AboutMe content and style

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,34 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from 'styled-components';
 import { FaCode, FaHome, FaRegHeart, FaUser } from "react-icons/fa";
 import { Table } from 'reactstrap';
 
-// style inline bc styled components not overriding style for th || tr
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-class AboutMe extends React.Component {
+const StyledTable = styled(Table)`
+  td {
+    vertical-align: middle;
+  }
+`;
+
+class AboutMe extends Component {
   render() {
     return (
-      <Table borderless className='mb-0'>
-        <thead>
-          <tr>
-            <th style={{verticalAlign:'middle'}}><FaUser size='1.5em'/></th>
-            <th><em>I’m a full-stack developer seeking opportunities in the Bay Area.</em></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td style={{verticalAlign:'middle'}}><FaCode size='1.5em'/></td>
-            <td>As a software developer, I love building and improving web apps, solving puzzles, and learning new concepts.</td>
-          </tr>
-          <tr>
-            <td style={{verticalAlign:'middle'}}><FaHome size='1.5em'/></td>
-            <td>When I'm not coding, I like playing guitar (poorly), playing tennis (pretty well), and walking leisurely outdoors (i.e. hiking).</td>
-          </tr>
-          <tr>
-            <td style={{verticalAlign:'middle'}}><FaRegHeart size='1.5em'/></td>
-            <td>At heart, I'm a builder and a problem solver with a passion for learning and growing.</td>
-          </tr>
-        </tbody>
-      </Table>
+      <Container>
+        <StyledTable borderless className='mb-0'>
+          <tbody>
+            <tr>
+              <td><FaUser size='1.5em'/></td>
+              <td><strong><em>I’m a versatile Frontend Engineer seeking opportunities in the Bay Area.</em></strong></td>
+            </tr>
+            <tr>
+              <td><FaCode size='1.5em'/></td>
+              <td>As a developer, I'm effective at combining problem-solving and creativity to craft polished apps with seamless UX.
+              I'm a quick and avid learner with a passion to build and fix things, and always driven to make an impact.</td>
+            </tr>
+            <tr>
+              <td><FaHome size='1.5em'/></td>
+              <td>When I'm not coding, I play guitar (poorly) and tennis (pretty well), I bike around SF, and walk leisurely outdoors (i.e. hiking).</td>
+            </tr>
+            <tr>
+              <td><FaRegHeart size='1.5em'/></td>
+              <td>At heart, I'm a builder and a problem solver with a passion for learning and growing.</td>
+            </tr>
+          </tbody>
+        </StyledTable>
+      </Container>
     );
   }
 }
