c40ea64109e316f4d27ae013a2afce7b7c143c0c
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-07-29 13:24:27 -0700

    added directions for updating resume sources

--- src/pages/resume/resumeSource.js
+++ src/pages/resume/resumeSource.js
@@ -1,6 +1,20 @@
 
-const googleDocId = '1-t1PbvbBT9YB4h-vRnGPKaaZaxIpz5ws';
+const googleDocId = '1seSYvfp2RkWnX-dQxWWO4TOnEd1QRiNY';
 export const resumeJpgLink = `https://drive.google.com/uc?id=${googleDocId}`;
-export const resumePdfLink = 'https://www.dropbox.com/s/t68bnf67x0i5o05/NCS_Resume_062420.pdf?dl=0';
+export const resumePdfLink = 'https://www.dropbox.com/s/xbficl7lkgt4g6a/NCS_Resume_072920.pdf?dl=0';
+
+/** Steps:
+ * PDF
+ *  - Upload new resume here: https://www.dropbox.com/home/Resume
+ *  - Click Share, then copy link for "Anyone can view"
+ *  - Replace `resumePdfLink` with that link
+ * 
+ * JPG
+ *  - Convert PDF to JPG here: https://pdf2jpg.net/
+ *  - Download the JPG and upload to "/public" here: https://drive.google.com/drive/u/0/folders/1Ln4e8SD0EiMlpwEzIs-pBYN3qJnH1HbP
+ *  - Click on the file, hamburger dropdown to "Open in New Window"
+ *  - Take the ID (i.e. in between /d/ and /view), paste in as `googleDocId`
+ *  
+ */
 
 // export const resumeJpgLink = 'https://i.imgur.com/2LUIwxM.jpg'; // alt link on imgur
