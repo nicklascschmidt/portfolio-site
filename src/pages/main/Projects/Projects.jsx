df73992c77d969df4b6ee8514de74dde190a73a4
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2020-10-02 12:00:46 -0700

    added Projects page

---
+++ src/pages/main/Projects/Projects.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { LinkContainer } from 'react-router-bootstrap';
+import styled from 'styled-components';
+import { Button } from 'reactstrap';
+
+import SectionHeader from '../components/SectionHeader.jsx';
+import ProjectCards from '../components/ProjectCards.jsx';
+import SectionFooter from '../../../components/sectionFooter/SectionFooter';
+
+const Container = styled.section`
+  background-image: url('palace_of_fine_arts.jpeg');
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-color: var(--link-color);
+  background-blend-mode: lighten;
+
+  & > *:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
+
+class Projects extends Component {
+  render() {
+    return (
+      <Container id={this.props.id}>
+        <SectionHeader isWhite title='Projects' id='projects-section'/>
+        <ProjectCards />
+        <SectionFooter>
+          <LinkContainer to='/projects'>
+            <Button>See More Projects</Button>
+          </LinkContainer>
+        </SectionFooter>
+      </Container>
+    );
+  }
+}
+
+export default Projects;
